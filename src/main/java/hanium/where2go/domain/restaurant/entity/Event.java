308ad05a5951bd3ddcc104911fc2dd91ef013133
jay_010304 <glp0509@naver.com>
2023-07-22 22:26:11 +0900

    [Feat]#36_common_restaurant_information_2

--- src/main/java/hanium/where2go/domain/restaurant/entity/Event.java
+++ src/main/java/hanium/where2go/domain/restaurant/entity/Event.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Builder
@@ -26,4 +28,8 @@ public class Event extends BaseEntity {
 
     private String title;
     private String content;
+
+    //레스토랑의 이벤트의 시작날짜와 끝나는 날짜 추가했음. 해당 날짜에 맞는 이벤트를 필터링해서 보여줘야 하기 때문
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 }
