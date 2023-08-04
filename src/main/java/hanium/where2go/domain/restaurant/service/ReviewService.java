6754ebbc68442f201c8a71020db00e7e05b82dda
jay_010304 <glp0509@naver.com>
2023-08-04 10:45:37 +0900

    [Feat] #49 레스토랑 해시태그 추출 쿼리문으로 변경

--- src/main/java/hanium/where2go/domain/restaurant/service/ReviewService.java
+++ src/main/java/hanium/where2go/domain/restaurant/service/ReviewService.java
@@ -3,8 +3,7 @@ package hanium.where2go.domain.restaurant.service;
 
 import hanium.where2go.domain.reservation.dto.HashtagResponseDto;
 import hanium.where2go.domain.reservation.dto.ReviewResponseDto;
-import hanium.where2go.domain.reservation.entity.Review;
-import hanium.where2go.domain.reservation.entity.ReviewHashtag;
+import hanium.where2go.domain.reservation.entity.Hashtag;
 import hanium.where2go.domain.restaurant.entity.Restaurant;
 import hanium.where2go.domain.restaurant.repository.RestaurantRepository;
 import hanium.where2go.domain.restaurant.repository.ReviewRepository;
@@ -14,9 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,6 +23,7 @@ import java.util.stream.Collectors;
 public class ReviewService {
     private final RestaurantRepository restaurantRepository;
     private final ReviewRepository reviewRepository;
+
 
 
     // 레스토랑의 리뷰 조회
@@ -50,22 +49,21 @@ public class ReviewService {
         Restaurant restaurant = restaurantRepository.findById(restaurantId)
                 .orElseThrow(() -> new BaseException(ExceptionCode.RESTAURANT_NOT_FOUND));
 
-        Map<String,Long> hashtagCountMap = new HashMap<>();
+        List<Object[]> top3Hashtag = reviewRepository.findTop3Hashtags();
+        List<String> top3HashtagNames = new ArrayList<>();
 
-        for(Review review : restaurant.getReview()){
-            for(ReviewHashtag reviewHashtag : review.getReviewHashtags()){
-                String hashtagName = reviewHashtag.getHashtag().getHashtagName();
-                hashtagCountMap.put(hashtagName,hashtagCountMap.getOrDefault(hashtagName,0L)+1);
+        int count = 0;
+        for (Object[] data : top3Hashtag) { // top 3 를 뽑아야 하는데 빈도수가 같은 해시태그가 여러 개 나올 수 있기 때문에 3개로 제한
+            String hashtagName = (String) data[0];
+            if (!top3HashtagNames.contains(hashtagName)) {
+                top3HashtagNames.add(hashtagName);
+                count++;
+            }
+            if (count == 3) { // 3개로 제한
+                break;
             }
         }
-
-        List<String> top3Hashtags = hashtagCountMap.entrySet().stream()
-                .sorted(Map.Entry.<String,Long>comparingByValue().reversed())
-                .limit(3)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-
-        return new HashtagResponseDto(top3Hashtags);
+        return new HashtagResponseDto(top3HashtagNames);
     }
 
 }
