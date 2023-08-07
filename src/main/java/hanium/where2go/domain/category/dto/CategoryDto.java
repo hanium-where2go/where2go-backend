85b9d3e627936ad99aff9d022fae629f2c5244c5
barded1998 <barded1998@gmail.com>
2023-08-07 22:35:16 +0900

    [Fix] dto builder 오류 수정

--- src/main/java/hanium/where2go/domain/category/dto/CategoryDto.java
+++ src/main/java/hanium/where2go/domain/category/dto/CategoryDto.java
@@ -1,11 +1,15 @@
 package hanium.where2go.domain.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
 
     private Long categoryId;
--- src/main/java/hanium/where2go/domain/liquor/dto/LiquorDto.java
+++ src/main/java/hanium/where2go/domain/liquor/dto/LiquorDto.java
@@ -1,11 +1,15 @@
 package hanium.where2go.domain.liquor.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LiquorDto {
 
     private Long liquorId;
