15bb45a63f9776498ca6fc9fb10b762c60b493ad
barded1998 <barded1998@gmail.com>
2023-08-15 23:07:14 +0900

    updated

---
+++ src/main/java/hanium/where2go/domain/HelloController.java
@@ -0,0 +1,12 @@
+package hanium.where2go.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
