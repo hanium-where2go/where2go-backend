090fb1c27f1b103a95e85c322632345e839fe6f8
jay_010304 <glp0509@naver.com>
2023-09-02 10:49:04 +0900

    [Remove]#78 소켓 구현 삭제

--- src/main/java/hanium/where2go/domain/reservation/controller/ReservationController.java
+++ src/main/java/hanium/where2go/domain/reservation/controller/ReservationController.java
@@ -2,17 +2,13 @@ package hanium.where2go.domain.reservation.controller;
 
 import hanium.where2go.domain.reservation.dto.HashtagResponseDto;
 import hanium.where2go.domain.reservation.dto.ReviewResponseDto;
-import hanium.where2go.domain.reservation.entity.ReservationStatus;
 import hanium.where2go.domain.restaurant.dto.ReservationDto;
-import hanium.where2go.domain.restaurant.service.ReservationService;
 import hanium.where2go.domain.restaurant.service.RestaurantService;
 import hanium.where2go.domain.restaurant.service.ReviewService;
 import hanium.where2go.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,18 +19,8 @@ public class ReservationController {
 
     private final ReviewService reviewService;
     private final RestaurantService restaurantService;
-    private final ReservationService reservationService;
 
-    // 예약 상세 내역 조회
-    @GetMapping("reservations/{reservationId}")
-    public ResponseEntity<BaseResponse<ReservationDto.ReservationDetailResponseDto>> searchDetailReservation(@@PathVariable("reservationId") Long reservationId){
 
-        ReservationDto.ReservationDetailResponseDto reservationDetailResponseDto = reservationService.searchDetailReservation(reservationId);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(new BaseResponse<>(HttpStatus.OK.value(), "가게 예약 상세 정보를 불러왔습니다", reservationDetailResponseDto));
-    }
 
 
     @GetMapping("/restaurants/{restaurantId}/review")
