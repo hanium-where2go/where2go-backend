c9966300dd01af766f4e55357a807f823dae9e96
kimjiheej <glp0509@naver.com>
2023-09-25 23:51:04 +0900

    [Feat]#78_restaurantId 별 레디스 키 생성

--- src/main/java/hanium/where2go/domain/reservation/controller/SeatController.java
+++ src/main/java/hanium/where2go/domain/reservation/controller/SeatController.java
@@ -13,18 +13,19 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
+@RestController
 public class SeatController {
-    private final SimpMessageSendingOperations messagingTemplate;
-    private ObjectMapper objectMapper;
+
     private final RedisUtil redisUtil;
 
-
     // 처음에 사장님이 레스토랑의 아이디와 좌석수를 소켓으로 보내는 부분
+    // messagePayload 를 통해서 json 형식으로 레스토랑의 아이디와 좌석수를 보내주면 레디스에 레스토랑 아이디로 구분해서 좌석수가 등록됨.
     @MessageMapping("/seats")
     @SendTo("/sub/seats")
     public Seats message(@Payload Map<String, Object> messagePayload) {
@@ -41,8 +42,8 @@ public class SeatController {
                 //throw a BaseException(ExceptionCode.ALREADY_RESTAURANT_KEY_EXISTS);
             }
 
-            redisUtil.set(restaurantKey + ":storeStatus", "OPEN", 2 * 60 * 60 * 120); // 영업 상태
-            redisUtil.set(restaurantKey, String.valueOf(seats), 2 * 60 * 60 * 120); // 2시간(120분) 동안 유효
+            redisUtil.set(restaurantKey + ":storeStatus", "OPEN", 2*60*60*1000); // 영업 상태
+            redisUtil.set(restaurantKey, String.valueOf(seats), 2*60*60*1000); // 2시간(120분) 동안 유효
 
             // 잘 들어왔나 확인용
             Seats seatsObject = new Seats();
@@ -57,12 +58,13 @@ public class SeatController {
         }
     }
     // 남은 좌석수를 확인하는 로직
+    // messagePayload 를 이용해서 json 형식으로 /pub/restseats 를 할때에 restaurantId 를 같이 보내준다
     @MessageMapping("/restseats")
     @SendTo("/sub/restseats")
     public Integer restSeats(@Payload Map<String, Object> messagePayload) {
         try {
             // JSON 메시지에서 restaurantId를 추출
-            Long receivedRestaurantId = (Long) messagePayload.get("restaurantId");
+            Integer receivedRestaurantId = (Integer) messagePayload.get("restaurantId");
 
             // restaurantId를 이용하여 해당 가게의 레디스 상태 키 생성
             String restaurantKey = String.valueOf(receivedRestaurantId);
@@ -91,35 +93,30 @@ public class SeatController {
     }
 
     // 사장님이 가게 닫는 요청을 한다
+    // messagePayload 를 이용해서 json 형식으로 /pub/close 를 할때에 restaurantId 를 같이 보내준다
     @MessageMapping("/close")
     public void closeStore(@Payload Map<String,Object> messagePayload) {
-        try {
 
-            Long receivedRestaurantId = (Long) messagePayload.get("restaurantId");
-            String restaurantKey = String.valueOf(receivedRestaurantId);
+        Integer receivedRestaurantId = (Integer) messagePayload.get("restaurantId");
+        String restaurantKey = String.valueOf(receivedRestaurantId);
 
-            if (!(redisUtil.hasKey(restaurantKey))) {
-                throw new BaseException(ExceptionCode.CANNOT_FIND_RESTAURANT_KEY);
-            }
-            // 가게 상태를 CLOSED로 업데이트
-            redisUtil.set(restaurantKey+":storeStatus","CLOSED",2*60*60*1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // 가게 상태를 CLOSED로 업데이트
+        redisUtil.set(restaurantKey + ":storeStatus", "CLOSED", 2 * 60 * 60 * 1000);
+        redisUtil.delete(restaurantKey);
     }
 
     // 가게의 운영 상태를 볼 수 있다
+    // messagePayload 를 이용해서 json 형식으로 /pub/status 를 할때에 restaurantId 를 같이 보내준다
     @MessageMapping("/status")
     @SendTo("/sub/status")
     public String closeOperationStatus(@Payload Map<String,Object> messagePayload) {
-        try {
-            Long receivedRestaurantId = (Long) messagePayload.get("restaurantId");
+
+            Integer receivedRestaurantId = (Integer) messagePayload.get("restaurantId");
             String restaurantKey = String.valueOf(receivedRestaurantId);
 
             if (!(redisUtil.hasKey(restaurantKey))) {
-                throw new BaseException(ExceptionCode.CANNOT_FIND_RESTAURANT_KEY);
+                return "STORE CLOSED";
             }
-
 
             String storeOperationStatus = (String) redisUtil.get(restaurantKey+":storeStatus");
             if ("OPEN".equals(storeOperationStatus)) {
@@ -127,9 +124,6 @@ public class SeatController {
             } else {
                 return "STORE CLOSED";
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "ERROR"; // 예외 발생 시 에러 메시지 반환
-        }
+
     }
 }
